0442a9cf7870e8567e9c3b00759a9a0d67c79514
Twesha Mitra <twesha95@gmail.com>
2017-10-09 21:17:53 -0400

    refactored method and test

--- test/CitySimTest.java
+++ test/CitySimTest.java
@@ -11,10 +11,13 @@ public class CitySimTest{
         Location l_2 = mock(Location.class);
         when(l_1.getLocationName()).thenReturn("Hotel");
         when(l_2.getLocationName()).thenReturn("Library");
-        Street s_1 = mock(Street.class);
-        assertEquals(test_city_sim.getCurrentStreet(l_1, l_2).getStreetName(), "Bill St");
+        assertEquals(test_city_sim.getCurrentStreet(l_1), "Bill St");
     }
 
+    /* I had some trouble with this test. I know that it's not ideal to call a method 
+     * on a returned object, but I had some trouble refactoring the method in CitySim.java.
+     * This probably happened due to the fact that it's slight hard coded. 
+     */
     @Test
     public void testGetCurrentAvenue(){
         Location l_1 = mock(Location.class);
